59b20a456d315f4352394a01172a2c894a68a58b
Corama Yunus <coramayunus@gmail.com>
2021-02-21 14:28:35 +0300

    Set basic mvc structure and add some basic files

---
+++ controllers/article.controller.js
@@ -0,0 +1,14 @@
+const Article = require('../models/article.model');
+exports.postArticle = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+exports.getArticles = async (req, res, next) => {
+  try {
+    res.send("<h3>Girls articles page</h3>");
+  } catch (error) {
+    next(error);
+  }
+};
