9a70bfd3dc8828434d9fce79c08698d1d76e478a
Corama Yunus <coramayunus@gmail.com>
2021-02-21 14:27:40 +0300

    Add basic app layout, middlewares and install dependencies

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config();
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 3000;
+const articleRoute = require("./routes/article.route");
+
+//MIDDLEWARES
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+//ROUTES
+app.use("/article", articleRoute);
+
+app.use("/", (req, res, next) => {
+  res.status(200).json({ message: "Welcome to Smartgirls in ICT" });
+  next;
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
+});
+mongoose.connect(
+  MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (error) => {
+    if (error) {
+      console.error(error);
+    }
+    app.listen(PORT, () => {
+      console.log(`Application Up and running on PORT: ${PORT}`);
+    });
+  }
+);
