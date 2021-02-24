32c48732763381bd6129a25af9b0e8956ddcdc91
Muneersahel <saidmunir73@gmail.com>
2021-02-24 12:54:09 +0300

    create activities view and add activities route

--- controllers/pages.controller.js
+++ controllers/pages.controller.js
@@ -4,3 +4,6 @@ exports.getIndex = (req, res, next) => {
 exports.getAbout = (req, res, next) => {
     res.render('about');
 }
+exports.getActivities = (req, res, next) => {
+    res.render('activities');
+}
--- routes/smartgirls.route.js
+++ routes/smartgirls.route.js
@@ -3,5 +3,6 @@ const pages = require("../controllers/pages.controller");
 
 router.get("/", pages.getIndex);
 router.get("/about", pages.getAbout);
+router.get("/activities", pages.getActivities);
 
 module.exports = router;
