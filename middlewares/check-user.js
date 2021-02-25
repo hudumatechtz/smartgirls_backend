a386863131ceb424d0e5f6bee5145221b9e75707
Corama Yunus <coramayunus@gmail.com>
2021-02-25 19:51:16 +0300

    set session check logic for logged in users

---
+++ middlewares/check-user.js
@@ -0,0 +1,18 @@
+const User = require("../models/user.model");
+module.exports = async (req, res, next)=>{
+    try {
+        if(!req.session.user){
+            console.log("Here")
+            return next();
+        }
+        const user = await User.findById(req.session.user._id);
+        if(!user){
+            console.log("No user");
+            return next();
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        next();
+    }
+}
