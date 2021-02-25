9e193ba3aff99b2c679f73f5a15ec304d1eaea50
Corama Yunus <coramayunus@gmail.com>
2021-02-25 19:48:52 +0300

    Install dependencies and implement middlewares

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const session = require("express-session");
+const mongoDbStore = require("connect-mongodb-session")(session);
+const checkUser = require("./middlewares/check-user");
 require("dotenv").config();
 const MONGO_URI =
   process.env.MONGO_URI || "mongodb://localhost:27017/girls_in_ict";
@@ -9,7 +12,36 @@ const PORT = process.env.PORT || 3000;
 const articleRoute = require("./routes/article.route");
 const pagesRoute = require("./routes/smartgirls.route");
 const authroute = require("./routes/auth.route");
+const multer = require("multer");
 
+const store = new mongoDbStore(
+  {
+    uri: MONGO_URI,
+    collection: "session",
+  },
+  (err) => {
+    if (err) console.log(err);
+  }
+);
+
+const fileFilter = (req, file, cb) => {
+  if (
+    (file.mimetype === "image/jpg",
+    file.mimetype === "image/jpeg",
+    file.mimetype === "image/png")
+  ) {
+    return cb(null, true);
+  }
+  cb(null, false);
+};
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.filename);
+  },
+});
 //MIDDLEWARES
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,7 +49,18 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
-
+app.use(
+  multer({ fileFilter: fileFilter, storage: fileStorage }).single("image")
+);
+app.use(
+  session({
+    secret: "callback wizard",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(checkUser);
 //ROUTES
 app.use("/article", articleRoute);
 app.use(pagesRoute);
