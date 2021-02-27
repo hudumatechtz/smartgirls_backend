29ed6aa53904862497579d53716e39bb197020c7
benfild <fildben@gmail.com>
2021-02-27 22:37:45 +0300

    articles links and routes fo normal and admin updated

--- routes/article.route.js
+++ routes/article.route.js
@@ -3,5 +3,5 @@ const article = require("../controllers/article.controller");
 
 router.get("/articles", article.getArticles);
 router.get("/articles-admin", article.getAdminArticles);
-router.get("/add-article", article.getAddArticle);
+router.get("/articles-admin/add-article", article.getAddArticle);
 module.exports = router;
