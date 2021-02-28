52f5346c5d0ca9766b457362371af1974135a222
Corama Yunus <coramayunus@gmail.com>
2021-02-28 18:45:54 +0300

    Improve error handling and conditioned the form

--- middlewares/check-user.js
+++ middlewares/check-user.js
@@ -1,19 +1,17 @@
 const User = require("../models/user.model");
-module.exports = async (req, res, next)=>{
-    try {
-        if(!req.session.user){
-            console.log("Here")
-            return next();
-        }
-        console.log('got user');
-        const user = await User.findById(req.session.user._id);
-        if(!user){
-            console.log("No user");
-            return next();
-        }
-        req.user = user;
-        next();
-    } catch (error) {
-        next();
+module.exports = async (req, res, next) => {
+  try {
+    if (!req.session.user) {
+      return next();
     }
-}
+    const user = await User.findById(req.session.user._id);
+    if (!user) {
+      console.log("No user");
+      return next();
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    next();
+  }
+};
