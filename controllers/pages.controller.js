4aae5d67d9e1511615619ea5590ca24bcc57bd94
Benfild <fildben@gmail.com>
2021-09-06 18:26:03 +0300

    limit to 3 home articles

--- controllers/pages.controller.js
+++ controllers/pages.controller.js
@@ -1,6 +1,14 @@
-exports.getIndex = (req, res, next) => {
-  res.render("home");
+const Article = require("../models/article.model");
+
+exports.getIndex = async (req, res, next) => {
+  const articles = await Article.find();
+  try {
+    res.render("home", { articles: articles });
+  } catch (error) {
+    next(error);
+  }
 };
+
 exports.getAbout = (req, res, next) => {
   res.render("about");
 };
@@ -8,21 +16,20 @@ exports.getActivities = (req, res, next) => {
   res.render("activities");
 };
 exports.getWorkforce = (req, res, next) => {
-    res.render('workforce');
-}
+  res.render("workforce");
+};
 exports.getGallery = (req, res, next) => {
-    res.render('gallery');
-}
+  res.render("gallery");
+};
 exports.getDashboard = (req, res, next) => {
   res.render("dashboard");
 };
-exports.getSchools = (req, res,next) => {
+exports.getSchools = (req, res, next) => {
   res.render("schools-admin");
-}
+};
 exports.getTrainers = (req, res, next) => {
   res.render("trainers-admin");
-}
+};
 exports.getCoachers = (req, res, next) => {
   res.render("coachers-admin");
-}
-
+};
