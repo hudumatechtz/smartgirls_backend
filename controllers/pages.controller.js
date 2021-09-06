cad22ac6296cae01c403488c5210314742979d90
Benfild <fildben@gmail.com>
2021-09-06 18:46:36 +0300

    clear article in db

--- controllers/pages.controller.js
+++ controllers/pages.controller.js
@@ -1,7 +1,7 @@
 const Article = require("../models/article.model");
 
 exports.getIndex = async (req, res, next) => {
-  Article.deleteMany();
+  Article.deleteMany({ title: "Bread London" });
   const articles = await Article.find();
   try {
     res.render("home", { articles: articles });
