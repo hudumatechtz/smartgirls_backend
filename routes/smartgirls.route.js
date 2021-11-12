fa83f7ea4d5530308c0d1cee748f0b2bc23ecb23
Muneersahel <saidmunir73@gmail.com>
2021-11-13 01:08:27 +0300

    modified colors and fix article content innerHtml

--- routes/smartgirls.route.js
+++ routes/smartgirls.route.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const pages = require("../controllers/pages.controller");
 
 router.get("/", pages.getIndex);
+router.get("/home", pages.getIndex);
 router.get("/about", pages.getAbout);
 router.get("/activities", pages.getActivities);
 router.get("/workforce", pages.getWorkforce);
 router.get("/gallery", pages.getGallery);
 router.get("/dashboard", pages.getDashboard);
-
 
 module.exports = router;
