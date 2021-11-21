db17abf746ea02619ac9f3f5e01437cf0758ba7d
benfild <fildben@gmail.com>
2021-11-21 15:22:54 -0500

    create: adding isLoggedIn middleware

---
+++ middlewares/index.js
@@ -0,0 +1,35 @@
+const User = require("../models/user.model");
+const middlewareObj = {};
+
+middlewareObj.getUser = async (req, res, next) => {
+  try {
+    if (!req.session.user) {
+      return next();
+    }
+    const user = await User.findById(req.session.user._id);
+    if (!user) {
+      console.log("No user");
+      return next();
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    next();
+  }
+};
+
+middlewareObj.isLoggedIn = async (req, res, next) => {
+  try {
+    if (req.session.isLoggedIn) {
+      return next();
+    }
+    // req.flash("error", "You need to be logged in to do that!");
+    message = "You need to be logged in to do that!";
+      // res.render("login", { message: message });
+    res.redirect("/account/login");
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = middlewareObj;
--- routes/smartgirls.route.js
+++ routes/smartgirls.route.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const pages = require("../controllers/pages.controller");
+const middleware = require("../middlewares/index");
 
 router.get("/", pages.getIndex);
 router.get("/home", pages.getIndex);
@@ -7,6 +8,6 @@ router.get("/about", pages.getAbout);
 router.get("/activities", pages.getActivities);
 router.get("/workforce", pages.getWorkforce);
 router.get("/gallery", pages.getGallery);
-router.get("/dashboard", pages.getDashboard);
+router.get("/dashboard", middleware.isLoggedIn ,pages.getDashboard);
 
 module.exports = router;
