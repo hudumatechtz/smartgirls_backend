05777368a5eb00d6ec9a7718af9ed3a24568e2ab
benfild <fildben@gmail.com>
2021-11-21 20:50:44 -0500

    modify: guard article-admin routes

--- routes/article.route.js
+++ routes/article.route.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const article = require("../controllers/article.controller");
+const middleware = require("../middlewares/index");
 
 router.get("/articles", article.getArticles);
-router.get("/articles-admin", article.getAdminArticles);
-router.get("/articles-admin/add-article", article.getAddArticle);
-router.post("/articles-admin/add-article", article.postArticle);
+router.get("/articles-admin", middleware.isLoggedIn, article.getAdminArticles);
+router.get("/articles-admin/add-article", middleware.isLoggedIn, article.getAddArticle);
+router.post("/articles-admin/add-article", middleware.isLoggedIn, article.postArticle);
 module.exports = router;
