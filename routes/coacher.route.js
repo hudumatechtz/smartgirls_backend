3fe24d9259bf2117eb39d36b7b62efce5e57eb75
benfild <fildben@gmail.com>
2021-11-21 21:19:33 -0500

    modify: guard coacher.routes

--- routes/coacher.route.js
+++ routes/coacher.route.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const coacher = require("../controllers/coacher.controller");
+const middleware = require("../middlewares/index");
 
 // router.get("/coachers", coacher.getCoachers);
-router.get("/coachers-admin", coacher.getAdminCoachers);
-router.get("/coachers-admin/add-coacher", coacher.getAddCoacher);
-router.post("/coachers-admin/add-coacher", coacher.postAddCoacher);
-router.get("/coachers-admin/edit-coacher/:id", coacher.getEditCoacher);
-router.post("/coachers-admin/edit-coacher/:id", coacher.postEditCoacher);
-router.get("/coachers-admin/delete-coacher/:id", coacher.getDeleteCoacher);
+router.get("/coachers-admin", middleware.isLoggedIn, coacher.getAdminCoachers);
+router.get("/coachers-admin/add-coacher", middleware.isLoggedIn, coacher.getAddCoacher);
+router.post("/coachers-admin/add-coacher", middleware.isLoggedIn, coacher.postAddCoacher);
+router.get("/coachers-admin/edit-coacher/:id", middleware.isLoggedIn, coacher.getEditCoacher);
+router.post("/coachers-admin/edit-coacher/:id", middleware.isLoggedIn, coacher.postEditCoacher);
+router.get("/coachers-admin/delete-coacher/:id", middleware.isLoggedIn, coacher.getDeleteCoacher);
 module.exports = router;
