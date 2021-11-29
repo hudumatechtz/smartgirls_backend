25dbf268280febd630d0214b99c494a4107fd976
benfild <fildben@gmail.com>
2021-11-29 10:09:53 -0500

    update: activities-admin delete phase route

--- routes/phase.route.js
+++ routes/phase.route.js
@@ -8,5 +8,5 @@ router.get("/phases-admin/:id/add-phase", middleware.isLoggedIn, phase.getAddPha
 router.post("/phases-admin/:id/add-phase", middleware.isLoggedIn, phase.postAddPhase);
 router.get("/phases-admin/:id/edit-phase/:phase_id", middleware.isLoggedIn, phase.getEditPhase);
 router.post("/phases-admin/:id/edit-phase/:phase_id", middleware.isLoggedIn, phase.postEditPhase);
-router.get("/phases-admin/:id/delete-phase/:phase_id", middleware.isLoggedIn, phase.getDeletePhase);
+router.get("/phases-admin/delete-phase/:phase_id", middleware.isLoggedIn, phase.getDeletePhase);
 module.exports = router;
