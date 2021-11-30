8f351ec71dfd8d9ef20d052b85b7b94dffa729fe
benfild <fildben@gmail.com>
2021-11-29 22:47:06 -0500

    update: comment edit-article for now

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -40,7 +40,8 @@ exports.getArticles = async (req, res, next) => {
 exports.getAddArticle = (req, res, next) => {
   res.render("add-article", { message: "" });
 };
-exports.getEditArticle = (req, res, next) => {
+
+exports.getEditArticle = async(req, res, next) => {
   const message = "";
 
   const article = await Article.findById(req.params.id);
@@ -63,26 +64,35 @@ exports.getEditArticle = (req, res, next) => {
 };
 
 exports.postEditArticle = async (req, res, next) => {
-  const { title, description, link } = req.body;
+  
   let message = "";
   const article = await Article.findById(req.params.id);
   try {
+    if (!article) {
+      console.log("Article does not exist");
+      message = "Article does not exists, Choose or Add another.";
+      res.render("add-article", {
+        message: message
+      });
+    }
+
     if (!req.file) {
       const error = new Error("No image file was provided");
       error.statusCode = 422;
       return res.render("add-article", { message: error.message });
     }
-    const creator = req.user.username;
-    const imageUrl = req.file.path;
     
-      article.title = title;
-      article.description = description;
-      article.imageUrl = imageUrl;
-      article.creator = creator;
-      article.link = link;
+      article.title = req.body.title;
+      article.description = req.body.description;
+      article.imageUrl = req.file.path;
+      article.creator = req.user.username;
+      article.link = req.body.link;
     
-     article.save(async (err) => {
-          if (err) return console.log(err);
+     const savedArticle = await article.save();
+    if (!savedArticle) {
+      message = "Article could not be posted";
+      return res.render("edit-article", { message: message, article: article });
+    }
 
           console.log("Article Edited success");
           message = "Article was edited successfully, To view go to articles";
@@ -90,7 +100,7 @@ exports.postEditArticle = async (req, res, next) => {
             article:article,
             message: message
           });
-        });
+    
   } catch (error) {
     next(error);
   }
